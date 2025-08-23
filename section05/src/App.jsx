28c87552182bc3830f94e473650e6fcb1aa7f753
Jeong jinyoung <jjyoung2419@gmail.com>
2025-08-23 23:51:02 +0900

    5.6) State로 상태관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,25 +1,28 @@
 import './App.css'
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from './components/Button';
+import { useState } from 'react';
 
-function App() {  // App 컴포넌트 (부모 컴포넌트)
+function App() {
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
 
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  }
   return (
     <>
-      <Button {...buttonProps} />  {/* spread 연산 통해 전달 가능 */}
-      <Button text={"카페"} />
-      <Button text={"블로그"} >
-        <Header /> {/* 이렇게 자식 요소로 사용된 HTML 태그는children 이름의 props로 자동으로 전달 */}
-      </Button>
+    <div>
+      <h1>{light}</h1>
+      <button onClick={() => {
+        setLight(light === "ON" ? "OFF": "ON");
+      }}>
+        {light === "ON" ? "끄기" : "켜기"}
+      </button>
+    </div>
+    <div>
+      <h1>{count}</h1>
+        <button onClick={() => {
+          setCount(count + 1);
+        }}>
+          +
+        </button>
+    </div>
     </>
   )
 }
