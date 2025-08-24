013eb5ad5e1829077b182ba4d592547d83d927a2
Jeong jinyoung <jjyoung2419@gmail.com>
2025-08-25 01:31:40 +0900

    8.3) 기능 구현 준비하기

--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -3,7 +3,7 @@ import './Editor.css';
 const Editor = () => {
   return (
   <div className='Editor'>
-    <input placeholder='새로운 Todo..'/>
+    <input placeholder='새로운 Todo...'/>
     <button>추가</button>
   </div>
   );
