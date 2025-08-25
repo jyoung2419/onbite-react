e14eff0700b26b992103d5ec8d5be4f8c4773791
Jeong jinyoung <jjyoung2419@gmail.com>
2025-08-25 18:18:15 +0900

    10.2) useMemo와 연산 최적화

--- section10/src/components/List.jsx
+++ section10/src/components/List.jsx
@@ -1,6 +1,6 @@
 import './List.css';
 import TodoItem from './TodoItem';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 const List = ({ todos, onUpdate, onDelete }) => {
   const [search, setSearch] = useState("");
@@ -20,9 +20,37 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
   const filteredTodos = getFilteredData();
 
+  const {totalCount, doneCount, notDoneCount} =
+  useMemo(()=>{
+    console.log("getAnalyzedData 호출!!!");
+    const totalCount = todos.length;
+    const doneCount = todos.filter(
+      (todo) => todo.isDone
+    ).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount
+    };
+  }, [todos]);
+  // 의존성배열 : deps([])
+  // useMemo의 두 번째 인수(deps 배열)에 포함된 값이 바뀔 때만
+  // 첫 번째 인수로 전달한 콜백을 다시 실행하고,
+  // 그 결과를 메모이제이션(캐싱)해서 재사용한다.
+
+  // const {totalCount, doneCount, notDoneCount} =
+  //   getAnalyzedData();
+
   return (
     <div className='List'>
       <h4>Todo List 🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
       <input 
         value={search}
         onChange={onChangeSearch}
