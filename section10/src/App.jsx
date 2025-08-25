b8fe76035cbd22732631fa79b81feea350ed706e
Jeong jinyoung <jjyoung2419@gmail.com>
2025-08-25 18:46:23 +0900

    10.4) useCallback과 함수 재생성 방지

--- section10/src/App.jsx
+++ section10/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useState, useRef, useReducer } from 'react'
+import { useState, useRef, useReducer, useCallback } from 'react'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
@@ -48,7 +48,7 @@ function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3)
 
-  const onCreate = (content)=>{
+  const onCreate = useCallback((content)=>{
     dispatch({
       type: "CREATE",
       data: {
@@ -58,9 +58,9 @@ function App() {
         date: new Date().getTime(),
       }
     })
-  };
+  }, []);
 
-  const onUpdate = (targetId)=>{
+  const onUpdate = useCallback((targetId)=>{
     // todos State의 값들 중에
     // targetId와 일치하는 Id를 갖는 투두 아이템의 isDone 변경
 
@@ -69,15 +69,15 @@ function App() {
       type: 'UPDATE',
       targetId: targetId
     })
-  };
+  }, []);
 
-  const onDelete = (targetId) => {
+  const onDelete = useCallback((targetId) => {
     // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
     dispatch({
       type: 'DELETE',
       targetId: targetId
     })
-  };
+  }, [])
 
   return (
     <div className='App'>
