945daf129f6ae7f4371d979fd9ebccb95c70b79b
Jeong jinyoung <jjyoung2419@gmail.com>
2025-08-28 16:54:45 +0900

    11.2) Context 사용하기

---
+++ section11/src/App.jsx
@@ -0,0 +1,104 @@
+import './App.css'
+import { useState, useRef, useReducer, useCallback, createContext } from 'react'
+import Header from './components/Header'
+import Editor from './components/Editor'
+import List from './components/List'
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
+
+function reducer(state, action) {
+  switch(action.type){
+    case 'CREATE':
+      return [action.data, ...state];
+    case 'UPDATE':
+      return state.map((item)=>
+        item.id === action.targetId 
+        ? {...item, isDone: !item.isDone} 
+        : item
+      );
+    case 'DELETE':
+      return state.filter((item)=>
+      item.id !== action.targetId
+      );
+    default:
+      return state;
+  }
+}
+
+export const TodoContext = createContext(); 
+// App 컴포넌트가 리렌더링될 때마다 다시 생성될 필요가 없으니
+// 보통 컴포넌트 외부에 선언
+
+function App() {  
+  const [todos, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3)
+
+  const onCreate = useCallback((content)=>{
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      }
+    })
+  }, []);
+
+  const onUpdate = useCallback((targetId)=>{
+    // todos State의 값들 중에
+    // targetId와 일치하는 Id를 갖는 투두 아이템의 isDone 변경
+
+    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
+    dispatch({
+      type: 'UPDATE',
+      targetId: targetId
+    })
+  }, []);
+
+  const onDelete = useCallback((targetId) => {
+    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
+    dispatch({
+      type: 'DELETE',
+      targetId: targetId
+    })
+  }, [])
+
+  return (
+    <div className='App'>
+      <Header />
+      <TodoContext.Provider
+        value={{
+          todos,
+          onCreate,
+          onUpdate,
+          onDelete
+        }}
+      >
+      <Editor />
+      <List />
+      </TodoContext.Provider>
+    </div>
+  )
+}
+
+export default App
---
+++ section11/src/components/List.jsx
@@ -0,0 +1,70 @@
+import './List.css';
+import TodoItem from './TodoItem';
+import { useState, useMemo, useContext } from 'react';
+import { TodoContext } from '../App';
+
+const List = () => {
+  const {todos} = useContext(TodoContext)
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if(search === ""){
+      return todos;
+    }
+    return todos.filter((todo)=>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
+  const {totalCount, doneCount, notDoneCount} =
+  useMemo(()=>{
+    console.log("getAnalyzedData 호출!!!");
+    const totalCount = todos.length;
+    const doneCount = todos.filter(
+      (todo) => todo.isDone
+    ).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount
+    };
+  }, [todos]);
+  // 의존성배열 : deps([])
+  // useMemo의 두 번째 인수(deps 배열)에 포함된 값이 바뀔 때만
+  // 첫 번째 인수로 전달한 콜백을 다시 실행하고,
+  // 그 결과를 메모이제이션(캐싱)해서 재사용한다.
+
+  // const {totalCount, doneCount, notDoneCount} =
+  //   getAnalyzedData();
+
+  return (
+    <div className='List'>
+      <h4>Todo List 🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
+      <input 
+        value={search}
+        onChange={onChangeSearch}
+        placeholder='검색어를 입력하세요'
+      />
+      <div className='todos_wrapper'>
+        {filteredTodos.map((todo)=>{
+          return <TodoItem key={todo.id} {...todo}/>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
